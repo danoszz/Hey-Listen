824fbcf54a160cc742299c6469c35a86579d50de
danoszz <danoszz@gmail.com>
2016-11-17 15:01:48 +0100

    style(_links.scss): glitch on links

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,10 +1,10 @@
 <nav class="navigation" role="navigation">
   <ul class="menu">
   <li class="menu--item">
-    <a href="#">About</a>
+    <a class="glitch__link" href="#" data-text="About">About</a>
   </li>
     <li class="menu--item">
-      <a href="mailto:ivo@kbfl.nl">Contact</a>
+      <a class="glitch__link" href="mailto:ivo@kbfl.nl" data-text="Contact">Contact</a>
     </li>
   </ul>
 </nav>
