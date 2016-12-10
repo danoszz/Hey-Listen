f48290dd3e58beb9ffb904b49dcc55db22094aa9
danoszz <danoszz@gmail.com>
2016-12-10 18:26:31 +0100

    Responsive fixes + shoutout

--- site/snippets/about.php
+++ site/snippets/about.php
@@ -8,6 +8,7 @@
 			</div>
 			<div class="about">
 				<p class="about--text">It’s no coincidence that the publishing company Nervora chose Bonne Marque as their partners to launch Vogue Arabia. We had been moving towards clients of this stature. It was our time. In 2016, we completely embraced an ‘our way or no way’ bold approach.</p>
+			 	<a href="https://devign.it/" target="_blank" class="about--shoutout">made by devign</a>
 			</div>
 		</div>
 	</div>
